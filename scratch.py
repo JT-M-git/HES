d58d2eda4dc9bc0f1a729fbeb46818c5e59cb4a2
JT-M-git <78225755+JT-M-git@users.noreply.github.com>
2021-11-09 15:08:18 +0530

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -7,11 +7,10 @@ np.random.seed(2)
 dataset = pd.read_csv("train_final.csv")
 y = dataset["784"]
 X = dataset.drop(labels = ["784"], axis = 1)
-y.value_counts()
 X = X / 255.0
 X = X.values.reshape(-1,28,28,1)
 y = to_categorical(y, num_classes = 14)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1 , random_state = 2, stratify = y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2 , random_state = 2, stratify = y)
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.preprocessing.image import ImageDataGenerator
@@ -32,17 +31,12 @@ model.add(Dense(14, activation = "softmax"))
 model.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
 
 datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.1, # Randomly zoom image
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=False,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+        rotation_range=10,
+        zoom_range = 0.1,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=False,
+        vertical_flip=False)
 
 model.fit_generator(datagen.flow(X_train,y_train, batch_size=60),epochs = 10,validation_data = (X_train,y_train), verbose = 1, steps_per_epoch=X_train.shape[0] // 60)
 model.save("pro1.h5")
